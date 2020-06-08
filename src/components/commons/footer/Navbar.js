420174bc9d954238bb642ad2fe996eb1cb20fe1b
jerkodeur <jerome.potie@gmail.com>
2020-06-08 19:00:53 +0200

    Display of the four images with text in flex

--- src/components/commons/footer/Navbar.js
+++ src/components/commons/footer/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+
+import heart from '../../../images/footer-favoris-heart.svg'
+import family from '../../../images/footer-fam.svg'
+import plain from '../../../images/footer-plain.svg'
+import star from '../../../images/footer-star.svg'
+
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Hello</div>
+    <footer className='cont-flex-navbar'>
+      <div className='item-flex-navbar'><img alt='Un avion en papier' src={plain} />PARTAGER</div>
+      <div className='item-flex-navbar'><img alt='Un visage en smiley' src={family} />FAMILLE</div>
+      <div className='item-flex-navbar'><img alt='Un coeur' src={heart} />FAVORI</div>
+      <div className='item-flex-navbar'><img alt='Une étoile' src={star} />MOMENTS</div>
+    </footer>
   )
 }
 
