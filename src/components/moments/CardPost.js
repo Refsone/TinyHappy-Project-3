00fd8457b3b815ad06c6fa612d5809e3170f00e1
Auxence <auxence.blondel@gmail.com>
2020-06-09 21:30:13 +0200

    add favorite event

--- src/components/moments/CardPost.js
+++ src/components/moments/CardPost.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CardPost.css'
 
 import fullHeart from '../../images/favoris-heart-pink.svg'
 import emptyHeart from '../../images/favori-heart.svg'
 
 export default function CardPost (props) {
-  const { moment } = props
+  const { moment, marginStyle } = props
+  const [favorite, setFavorite] = useState(moment.moment_favorite)
+  const handleClickFavorite = () => setFavorite(!favorite)
+
   return (
-    <div className='CardPost'>
+    <div className='CardPost' style={marginStyle}>
       <div className='block-names-favorite'>
         <div>
           {moment.family_firstname.map((family, key) => <p className={`${family.color} family-name`} key={key}>{family.name}</p>)}
         </div>
-        <img src={moment.moment_favorite ? fullHeart : emptyHeart} alt='favorite' />
+        <img onClick={handleClickFavorite} src={favorite ? fullHeart : emptyHeart} alt='favorite' />
       </div>
       <h4>{moment.moment_text}</h4>
       {moment.moment_context !== '' ? <p className='context'>{moment.moment_context}</p> : ''}
--- src/components/moments/Posts.js
+++ src/components/moments/Posts.js
@@ -18,7 +18,7 @@ export default function Post () {
       user_isPresent: 0,
       moment_text: 'Les filles ont fabriqué une cabane avec tous les éléments mobiles de l’appart. 30 minutes de rangement, youpi.',
       moment_context: '',
-      moment_event_date: '2020-09-12',
+      moment_event_date: '2020-05-09',
       moment_favorite: 1,
       user_id: 1,
       moment_type_id: 1,
@@ -29,7 +29,7 @@ export default function Post () {
       user_isPresent: 0,
       moment_text: 'Les filles sont chez les grands-parents !!!! Liberés, délivrés !',
       moment_context: 'En parlant à sa soeur qui est un peu bruyante.',
-      moment_event_date: '2020-05-12',
+      moment_event_date: '2020-05-09',
       moment_favorite: 0,
       user_id: 1,
       moment_type_id: 0,
@@ -56,9 +56,12 @@ export default function Post () {
       family_firstname: [{ name: 'Louise', color: 'cyan' }]
     }
   ])
+
   return (
     <div className='Post'>
-      {moments.map((moment, key) => <CardPost moment={moment} key={key} />)}
+      {moments.map((moment, key) => {
+        return <CardPost moment={moment} key={key} marginStyle={{ marginTop: '40px' }} />
+      })}
     </div>
   )
 }
