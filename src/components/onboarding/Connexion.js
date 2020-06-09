3c243c427b42b2fb56c50f40979cc14835fb043a
Moni <monica_be@hotmail.fr>
2020-06-09 13:10:27 +0200

    separation general css

--- src/components/onboarding/Connexion.js
+++ src/components/onboarding/Connexion.js
@@ -6,13 +6,13 @@ class Connexion extends Component {
   render () {
     return (
       <div className='connexion-background'>
-        <div className='connexion-container'>
+        <div className='form-container'>
           <Logo />
-          <form className='connexion-form'>
-            <label htmlFor='email' name='email' className='connexion-label'>email</label>
-            <input name='email' type='text' className='connexion-input plholder' placeholder='mon@email.com' required />
-            <label htmlFor='password' className='connexion-label'>mot de passe</label>
-            <input name='password' type='password' className='connexion-input mdp' placeholder='**********' required />
+          <form className='general-form'>
+            <label htmlFor='email' name='email' className='general-label'>email</label>
+            <input name='email' type='text' className='general-input plholder' placeholder='mon@email.com' required />
+            <label htmlFor='password' className='general-label'>mot de passe</label>
+            <input name='password' type='password' className='general-input mdp-forbiden' placeholder='**********' required />
             <p className='connexion-lien'><a href='#'>Mot de passe perdu ?</a></p>
             <button type='button' className='btn-connexion'>se connecter</button>
           </form>
