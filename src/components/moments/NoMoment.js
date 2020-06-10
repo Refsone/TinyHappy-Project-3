f6b6d8cfc59b0de40e418df863f7bcb83240b031
jerkodeur <jerome.potie@gmail.com>
2020-06-10 16:03:38 +0200

    Add css to the text and fix class error in the navbar

--- src/components/moments/NoMoment.js
+++ src/components/moments/NoMoment.js
@@ -8,8 +8,8 @@ const NoMoment = () => {
     <>
       <div className='cont-noMoment'>
         <img alt='A smiley Mailbox' src={mailBox} />
-        <p>Oups ! Il semblerait que vous n'ayez encore rien posté !</p>
-        <p>Il est temps d'écrire votre histoire, créez votre premier moment !</p>
+        <p className='bold-20px-grey'>Oups ! Il semblerait que vous n'ayez encore rien posté !</p>
+        <p className='regular-16px-grey'>Il est temps d'écrire votre histoire, créez votre premier moment !</p>
       </div>
     </>
   )
