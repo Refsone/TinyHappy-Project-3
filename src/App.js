377561f2f333adbd01371357cf636583b79211bd
Moni <monica_be@hotmail.fr>
2020-06-10 13:41:55 +0200

    cleaning comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from './components/commons/header/LogoHeader'
 
 import './App.css'
 
 function App () {
   return (
-    <p>Ici avait le compo logo pour le voir efface-moi !</p>
+    <Logo />
   )
 }
 
