f1750979bc72b8c64bb7fb5e504bc3c880f8f7e0
Auxence <auxence.blondel@gmail.com>
2020-06-10 17:00:17 +0200

    create Member and CardMembers component

---
+++ src/components/family/CardMembers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './Member'
+import './CardMembers.css'
+
+export default function CardMembers () {
+  return (
+    <>
+      <Member />
+    </>
+  )
+}
