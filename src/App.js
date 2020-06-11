2c988c8417ccfc61c441b2496a3a5881a47c29c5
Moni <monica_be@hotmail.fr>
2020-06-11 11:01:29 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import CardMembers from './components/family/CardMembers'
-import './App.css'
-// import Access from './components/onboarding/Access'
-// import MomentNavbar from './components/moments/MomentNavbar'
+import Access from './components/onboarding/Access'
+import MomentNavbar from './components/moments/MomentNavbar'
 import Logo from './components/commons/header/LogoHeader'
-
-
 import Navbar from 'src/components/commons/footer/Navbar'
+import CardContacts from './components/share/CardContacts'
+import SignUp from './components/onboarding/SignUp'
 
+import './App.css'
 import '../src/components/commons/Fonts.css'
 
 function App () {
   return (
     <div className='App'>
       <CardMembers />
-      {/* <Access /> */}
-      {/* <MomentNavbar /> */}
-      <Logo />
+      <Access />
+      <MomentNavbar />
       <CardContacts />
       <SignUp />
+      <Logo />
       <Navbar />
     </div>
   )
