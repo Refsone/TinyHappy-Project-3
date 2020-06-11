b5957748aa0348f87c497183fc17da98785a3a24
MANAUT <tristanmanaut@gmail.com>
2020-06-11 10:54:35 +0200

    component sign up fonctionnal ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignUp from './components/onboarding/SignUp'
+
 import './App.css'
 
 function App () {
   return (
     <div className='App'>
       <SignUp />
+     
     </div>
   )
 }
