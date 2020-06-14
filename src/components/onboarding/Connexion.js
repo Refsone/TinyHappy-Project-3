df9e562ea9703e268d4a6629d3bebf899032a873
Moni <monica_be@hotmail.fr>
2020-06-14 15:25:03 +0200

    touching css

--- src/components/onboarding/Connexion.js
+++ src/components/onboarding/Connexion.js
@@ -15,16 +15,17 @@ const Connexion = () => {
       <Logo />
       <form onSubmit={handleSubmit} action='/' className='general-form-connexion' noValidate>
         <label htmlFor='user_mail' name='user_mail' className='label-connexion'>email</label>
-        <input name='user_mail' type='email' id='user_mail' className='input-connexion plholder bold-12px-grey' placeholder='mon@email.com' value={values.email} onChange={handleChange} />
+        <input name='user_mail' type='email' id='user_mail' className={`${errors.user_mail ? 'input-connexion-error' : 'input-connexion plholder bold-12px-grey'}`} placeholder='mon@email.com' value={values.email} onChange={handleChange} />
 
         {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
 
         <label htmlFor='user_password' className='label-connexion'>mot de passe</label>
-        <input name='user_password' type='password' id='user_password' className='input-connexion mdp-forbiden plholder bold-12px-grey' placeholder='**********' value={values.password} onChange={handleChange} />
+        <input name='user_password' type='password' id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-connexion mdp-open plholder bold-12px-grey'}`} placeholder='**********' />
         {errors.user_password && <p className='msg-error'>{errors.user_password}</p>}
 
         <p className='connexion-lien'><a href='/'>Mot de passe perdu ?</a></p>
-        <button type='submit' className={`${errors.user_mail || errors.user_password ? 'connexion-btn inactif' : 'connexion-btn actif'}`}>se connecter</button>
+
+        <button type='submit' className={`${!handleSubmit ? 'connexion-btn inactif' : 'connexion-btn actif'}`}>se connecter</button>
       </form>
     </div>
   )
