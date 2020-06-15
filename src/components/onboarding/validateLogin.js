79d435580dc5c21fd13cfc65d510181855406226
Moni <monica_be@hotmail.fr>
2020-06-15 11:16:03 +0200

    changing to french

--- src/components/onboarding/validateLogin.js
+++ src/components/onboarding/validateLogin.js
@@ -1,14 +1,14 @@
 function validationLogIn (values) {
   let errors = {}
   if (!values.user_mail) {
-    errors.user_mail = 'Email is required!'
+    errors.user_mail = 'Votre email est requis!'
   } else if (!/\S+@\S+\.\S+/.test(values.user_mail)) {
     errors.user_mail = 'Email addresse in invalid'
   }
   if (!values.user_password) {
-    errors.user_password = 'Password is required!'
-  } else if (values.user_password.length < 10) {
-    errors.user_password = 'Password must have more than 10 characters'
+    errors.user_password = 'Votre mot de passe est requis !'
+  } else if (values.user_password.length < 8) {
+    errors.user_password = 'Votre mot de passe doit avoir plus huit charactères'
   }
   return errors
 }
