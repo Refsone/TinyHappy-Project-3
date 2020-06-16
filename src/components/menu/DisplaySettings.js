f4a033bea89e6bb9b52366cef575ec3c93a4503b
Moni <monica_be@hotmail.fr>
2020-06-16 21:29:52 +0200

    doing links

--- src/components/menu/DisplaySettings.js
+++ src/components/menu/DisplaySettings.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react'
-import LogoHeaderMoments from './../commons/header/LogoHeaderMoments'
+import React from 'react'
+import Header from './../commons/header/Header'
 
 import './DisplaySettings.css'
 
-class DisplaySettings extends Component {
-  render () {
-    return (
-      <>
-        <LogoHeaderMoments />
-        <div className='settings-container'>
-          <h1 className='settings-title bold-16px-grey'>paramètres </h1>
-          <form className='general-connexion-form'>
-            <label className='settings-soutitle bold-12px-grey'>Application</label>
-            <input className='general-input' placeholder='Afficher les âges des membres' />
-            <label className='settings-soutitle bold-12px-grey'>Sécurité</label>
-            <input className='general-input' placeholder='Modifier votre mot de passe' />
-            <input className='general-input' placeholder='Modifier votre adresse email' />
-          </form>
-        </div>
-      </>
-    )
-  }
+const DisplaySettings = (props) => {
+  const path = props.location.pathname
+  return (
+    <>
+      <Header location={path} burger />
+      <div className='settings-container'>
+        <h1 className='settings-title bold-16px-grey'>paramètres</h1>
+        <h2 className='settings-soutitle bold-12px-grey'>Application</h2>
+        <p className='settings-pseudoinput medium-12px-lightgrey'>Afficher les âges des membres</p>
+        <h2 className='settings-soutitle bold-12px-grey'>Sécurité</h2>
+        <p className='settings-pseudoinput medium-12px-lightgrey'>Modifier votre mot de passe</p>
+        <p className='settings-pseudoinput medium-12px-lightgrey'>Modifier votre adresse mail</p>
+      </div>
+    </>
+  )
 }
 
 export default DisplaySettings
