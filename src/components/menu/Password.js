b76fedf5d43e68c25b93453edc6f6ff6f2d5d21f
Moni <monica_be@hotmail.fr>
2020-06-18 00:33:30 +0200

    positionning eye

--- src/components/menu/Password.js
+++ src/components/menu/Password.js
@@ -6,20 +6,26 @@ import './../onboarding/Connexion.css'
 import './Password.css'
 
 const Password = () => {
-  const { handleChange, handleSubmit, values, errors } = useForm(submit, validationLogIn)
+  const { handleChange, handleSubmit, onclick, values, errors, showType } = useForm(submit, validationLogIn)
   function submit () {
     console.log('sent succesfully')
   }
+
   return (
     <div className='settings-container-pwdmail'>
       <h1 className='settings-pwmail-title bold-16px-grey'>modification du mot de passe</h1>
       <form onSubmit={handleSubmit} className='general-form-connexion' noValidate>
         <label htmlFor='user_password' name='user_password' className='label-settings bold-12px-grey'>Nouveau mot de passe</label>
-        <input name='user_password' type='password' id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-psw-default mdp-open plholder bold-12px-grey'}`} placeholder='**********' />
+        <button className={`${showType ? 'show-eye' : 'show-eye-open'}`} onClick={onclick} />
+
+        <input name='user_password' type={showType} id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-connexion plholder bold-12px-grey'}`} placeholder='**********' />
         {errors.user_password && <p className='msg-error'>{errors.user_password}</p>}
 
         <label htmlFor='user_password' className='label-settings bold-12px-grey'>confirmation du nouveau de passe</label>
-        <input name='user_password' type='password' id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-psw-default mdp-open plholder bold-12px-grey'}`} placeholder='**********' />
+
+        <button className={`${showType ? 'show-eye2' : 'show-eye-open2'}`} onClick={onclick} />
+
+        <input name='user_password' type={showType} id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-connexion plholder bold-12px-grey'}`} placeholder='**********' />
         {errors.user_password && <p className='msg-error'>{errors.user_password}</p>}
 
         {errors && values.user_password === '' ? <button type='submit' className='connexion-btn-inactif'>confirmer</button> : <button type='submit' className='connexion-btn-actif'>confirmer</button>}
--- src/components/onboarding/useForm.js
+++ src/components/onboarding/useForm.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
+import EyeClosed from '../../images/eye-slash-regular1.svg'
+import EyeOpen from '../../images/eye-open.svg'
 
 const useForm = (callback, validating) => {
   const [values, setValues] = useState({ user_mail: '', user_password: '' })
   const [errors, setErrors] = useState({})
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const [visible, setVisible] = useState(false)
   const handleChange = (event) => {
     const { name, value } = event.target
     setValues({ ...values, [name]: value })
@@ -19,9 +22,15 @@ const useForm = (callback, validating) => {
     }
   }, [errors])
 
+  const showType = visible ? 'text' : 'password'
+  const showIcon = (visible ? { EyeClosed } : { EyeOpen })
+  const onclick = () => { setVisible(visible => !visible) }
   return {
     handleChange,
     handleSubmit,
+    showType,
+    showIcon,
+    onclick,
     values,
     errors
   }
