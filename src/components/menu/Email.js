f48e767d6639d40e1b375150111bf417c940a433
Moni <monica_be@hotmail.fr>
2020-06-18 18:35:11 +0200

    updating email component

--- src/components/menu/Email.js
+++ src/components/menu/Email.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useForm from './../onboarding/useForm'
 import validationLogIn from './../onboarding/validateLogin'
 
+import eyeClosed from '../../images/eye-slash-regular1.svg'
+import eyeOpen from '../../images/eye-open.svg'
 import './../onboarding/Connexion.css'
 import './Password.css'
 
@@ -10,16 +12,26 @@ const Email = () => {
   function submit () {
     console.log('sent succesfully')
   }
+
+  const [visible, setVisible] = useState(false)
+  const showType = visible ? 'text' : 'email'
+
   return (
     <div className='settings-container-pwdmail'>
       <h1 className='settings-pwmail-title bold-16px-grey'>modification votre adresse email</h1>
       <form onSubmit={handleSubmit} className='general-form-connexion' noValidate>
         <label htmlFor='user_mail' name='user_mail' className='label-settings bold-12px-grey'>Nouvelle adresse mail</label>
-        <input name='user_mail' type='email' id='user_mail' value={values.email} onChange={handleChange} className={`${errors.user_mail ? 'input-pws-error' : 'input-psw-default mdp-open plholder bold-12px-grey'}`} placeholder='**********' />
+        <div className='settings-container-eye'>
+          <img src={visible ? eyeOpen : eyeClosed} onClick={() => setVisible(!visible)} />
+        </div>
+        <input name='user_mail' type={showType} id='user_mail' value={values.email} onChange={handleChange} className={`${errors.user_mail ? 'input-pws-error' : 'input-psw-default plholder bold-12px-grey'}`} placeholder='**********' />
         {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
 
         <label htmlFor='user_mail' className='label-settings bold-12px-grey'>confirmation de la nouvelle adresse mail</label>
-        <input name='user_mail' type='email' id='user_mail' value={values.email} onChange={handleChange} className={`${errors.user_mail ? 'input-pws-error' : 'input-psw-default mdp-open plholder bold-12px-grey'}`} placeholder='**********' />
+        <div className='settings-container-eye'>
+          <img src={visible ? eyeOpen : eyeClosed} onClick={() => setVisible(!visible)} />
+        </div>
+        <input name='user_mail' type={showType} id='user_mail' value={values.email} onChange={handleChange} className={`${errors.user_mail ? 'input-pws-error' : 'input-psw-default plholder bold-12px-grey'}`} placeholder='**********' />
         {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
 
         {errors && values.user_mail === '' ? <button type='submit' className='connexion-btn-inactif'>confirmer</button> : <button type='submit' className='connexion-btn-actif'>confirmer</button>}
--- src/components/onboarding/useForm.js
+++ src/components/onboarding/useForm.js
@@ -1,12 +1,9 @@
 import { useState, useEffect } from 'react'
-import EyeClosed from '../../images/eye-slash-regular1.svg'
-import EyeOpen from '../../images/eye-open.svg'
 
 const useForm = (callback, validating) => {
   const [values, setValues] = useState({ user_mail: '', user_password: '' })
   const [errors, setErrors] = useState({})
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const [visible, setVisible] = useState(false)
   const handleChange = (event) => {
     const { name, value } = event.target
     setValues({ ...values, [name]: value })
@@ -22,15 +19,9 @@ const useForm = (callback, validating) => {
     }
   }, [errors])
 
-  const showType = visible ? 'text' : 'password'
-  const showIcon = (visible ? { EyeClosed } : { EyeOpen })
-  const onclick = () => { setVisible(visible => !visible) }
   return {
     handleChange,
     handleSubmit,
-    showType,
-    showIcon,
-    onclick,
     values,
     errors
   }
