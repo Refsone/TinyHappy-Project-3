c12a78e604b4d1ac7a7a48045135d70197ebfbbc
Auxence <auxence.blondel@gmail.com>
2020-06-29 22:07:54 +0200

    add function to retrieve select authors and filter on date

--- src/components/share/AddShareMoment.js
+++ src/components/share/AddShareMoment.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Moment from 'moment'
+import 'moment/locale/fr'
 
 import Authors from '../commons/Authors'
 import Header from '../commons/header/Header'
@@ -11,6 +14,61 @@ import DateRange from './DateRange'
 function AddShareMoment (props) {
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
+  const [countSelect, setCountSelect] = useState(0)
+  const [family, setFamily] = useState([])
+  const [authors, setAuthor] = useState([])
+  const [authorsSelect, setAuthorSelect] = useState([])
+  const [moments, setMoments] = useState([])
+
+  useEffect(() => {
+    fetchMoments()
+    fetchFamily()
+  }, [])
+
+  useEffect(() => {
+    fetchUser()
+  }, [family])
+
+  const fetchFamily = (id = 1) => {
+    axios.get(`http://localhost:7500/users/${id}/family`)
+      .then(res => setFamily(res.data))
+  }
+
+  const fetchUser = (id = 1) => {
+    axios.get(`http://localhost:7500/users/${id}`)
+      .then(res => setAuthor(family.concat(res.data)))
+  }
+
+  const fetchMoments = (id = 1) => {
+    axios.get(`http://localhost:7500/users/${id}/moments`)
+      .then(res => setMoments(res.data))
+  }
+
+  const clickAuthor = (e) => {
+    const targetStyle = e.target.style
+    const id = e.target.id
+    //  If the button is not clicked (blank color)
+    if (targetStyle.backgroundColor === 'rgb(255, 255, 255)' || targetStyle.backgroundColor === '') {
+      targetStyle.backgroundColor = authors[id].color
+      setAuthorSelect([...authorsSelect, { name: authors[id].family_firstname, color: authors[id].color }])
+      // setAuthorSelect([...authorsSelect, { name: authors[id].family_firstname, color: authors[id].color }])
+      setCountSelect(countSelect + 1)
+    } else {
+      targetStyle.backgroundColor = '#fff'
+      const idToDelete = authorsSelect.findIndex(author => author.name === authors[id].family_firstname)
+      authorsSelect.splice(idToDelete, 1)
+      setAuthorSelect(authorsSelect)
+      setCountSelect(countSelect - 1)
+    }
+  }
+
+  const handleClick = () => {
+    Moment.locale('fr')
+    const momentsToSend = moments
+      .filter(moment => Moment(moment.moment_event_date).format('L') >= Moment(startDate).format('L') && Moment(moment.moment_event_date).format('L') <= Moment(endDate).format('L'))
+    // .filter(moment => moment.firstname_color.map())
+    console.log(momentsToSend)
+  }
 
   return (
     <>
@@ -19,10 +77,10 @@ function AddShareMoment (props) {
         <h1 className='bold-16px-grey'> Paramètre du partage</h1>
         <h2 className='bold-12px-grey'>Quels Auteur(e)s</h2>
         <div className='block-authors'>
-          <Authors />
+          <Authors clickAuthor={e => clickAuthor(e)} authors={authors} />
         </div>
         <h2 className='bold-12px-grey'>Quels type(s) de moments ?</h2>
-        <input id='quoteCheck' type='checkbox' />
+        <input id='quoteCheck' type='checkbox' checked />
         <label className='bold-10px-grey quote-check' htmlFor='quoteCheck'>Citation</label>
         <input id='milestoneCheck' type='checkbox' />
         <label className='bold-10px-grey milestone-check' htmlFor='milestoneCheck'>Fait notable</label>
@@ -31,7 +89,7 @@ function AddShareMoment (props) {
         <DateRange dateType='fin' date={endDate} onChange={date => setEndDate(date)} />
       </div>
 
-      <ValidateButton name='Envoyer les moments' />
+      <ValidateButton handleClick={handleClick} active={countSelect > 0 && startDate <= endDate} name='Envoyer les moments' />
     </>
   )
 }
