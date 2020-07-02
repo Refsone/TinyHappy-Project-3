ccae1ed346f8eb980fc0da5e558100192ce79914
Jerôme Potié <49954831+jerkodeur@users.noreply.github.com>
2020-07-02 18:15:07 +0200

    Update AddContact.js

--- src/components/share/AddContact.js
+++ src/components/share/AddContact.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import './AddContact.css'
-// import inactifSend from '../../images/send-inactif-btn.svg'
 
 const AddContact = ({ handleclick }) => {
   const [mail, setMail] = useState()
