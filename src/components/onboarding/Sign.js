671eafe4db8573d144c09b77ce696a30af91bcd1
Moni <monica_be@hotmail.fr>
2020-07-03 14:28:41 +0200

    redoing connexion

--- src/components/onboarding/Sign.js
+++ src/components/onboarding/Sign.js
@@ -4,32 +4,6 @@ import { Link } from 'react-router-dom'
 import './Sign.css'
 
 const Sign = () => {
-/*   const [userData, setUserData] = useState({
-    user: undefined,
-    token: undefined
-  })
-
-  useEffect(() => {
-    const checkLoggedIn = async () => {
-      let token = localStorage.getItem('auth-token')
-      if (token === null) {
-        localStorage.setItem('auth-token', '')
-        token = ''
-      }
-      const tokenRes = await axios.post('http://localhost:7500/users/login/tokenIsValid', null, { headers: { 'x-auth-token': token } })
-
-      if (tokenRes.data) {
-        const userRes = await axios.get('http://localhost:7500/users/login', { headers: { 'x-auth-token': token } })
-        setUserData({
-          token,
-          useEffect: userRes.data
-        })
-      }
-      console.log(tokenRes.data)
-    }
-    checkLoggedIn()
-  }, []) */
-
   return (
     <div className='sign'>
       <Link to='/onboarding/login' className='connect'>se connecter</Link>
