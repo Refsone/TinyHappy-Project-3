33cdc55a0577de1fe64fa82763d901de0388eec0
Moni <monica_be@hotmail.fr>
2020-07-05 15:05:13 +0200

    updating dev

--- src/components/onboarding/Connexion.js
+++ src/components/onboarding/Connexion.js
@@ -1,44 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import Header from '../commons/header/Header'
-import { useHistory } from 'react-router-dom'
 import useForm from './useForm'
 import validationLogIn from './validateLogin'
 
-
-import ValidateButton from '../commons/footer/ValidateButton'
+import eyeClosed from '../../images/eye-slash-regular1.svg'
+import eyeOpen from '../../images/eye-open.svg'
 
 import './Connexion.css'
 
 const Connexion = (props) => {
+  const { handleChange, handleSubmit, values, errors, setErrors } = useForm(submit, validationLogIn)
 
-  let history = useHistory()
-  const { handleChange, handleSubmit, values, errors } = useForm(submit, validationLogIn)
-  function submit() {
-    console.log('sent succesfully')
+  const [visible, setVisible] = useState(false)
+  const showType = visible ? 'text' : 'password'
+
+  async function submit (e) {
+    e.preventDefault()
+    console.log(values)
+    try {
+      await axios.post('http://localhost:7500/users/login', values)
+        .then(res => res.headers['x-access-token'])
+        .then(data => localStorage.setItem('x-access-token', data))
+    } catch (err) {
+      errors && setErrors(errors)
+    }
   }
 
-  const handleClick = () => history.push('/family')
-
   return (
-    <>
+    <div className='connexion-background'>
       <Header location={props.location.pathname} />
-      <div className='connexion-background'>
-        <form onSubmit={handleSubmit} className='general-form-connexion' noValidate>
-          <label htmlFor='user_mail' name='user_mail' className='label-connexion'>email</label>
-          <input name='user_mail' type='email' id='user_mail' className={`${errors.user_mail ? 'input-connexion-error' : 'input-connexion plholder bold-12px-grey'}`} placeholder='mon@email.com' value={values.email} onChange={handleChange} />
+      <form onSubmit={handleSubmit} className='general-form-connexion' noValidate>
+        <label htmlFor='user_mail' name='user_mail' className='label-connexion'>email</label>
+        <input name='user_mail' type='email' id='user_mail' className={`${errors.user_mail ? 'input-connexion-error' : 'input-connexion plholder bold-12px-grey'}`} placeholder='mon@email.com' value={values.email} onChange={handleChange} />
 
-          {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
+        {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
 
-          <label htmlFor='user_password' className='label-connexion'>mot de passe</label>
-          <input name='user_password' type='password' id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-psw-default mdp-forbiden plholder bold-12px-grey'}`} placeholder='**********' />
-          {errors.user_password && <p className='msg-error'>{errors.user_password}</p>}
-
-          <p className='connexion-lien'><a href='/'>Mot de passe perdu ?</a></p>
-          {/* {errors && values.user_mail === '' && values.user_password === '' ? <button type='submit' className='connexion-btn-inactif'>se connecter</button> : <button type='submit' className='connexion-btn-actif'>se connecter</button>} */}
-          <ValidateButton name='se connecter' handleClick={handleClick} active/>
-        </form>
-      </div>
-    </>
+        <label htmlFor='user_password' className='label-connexion'>mot de passe</label>
+        <div className='settings-container-eye'>
+          <img src={visible ? eyeOpen : eyeClosed} onClick={() => setVisible(!visible)} alt='' />
+        </div>
+        <input name='user_password' type={showType} id='user_password' value={values.password} onChange={handleChange} className={`${errors.user_password ? 'input-pws-error' : 'input-psw-default plholder bold-12px-grey'}`} placeholder='**********' />
+        {errors.user_password && <p className='msg-error'>{errors.user_password}</p>}
+        <p className='connexion-lien'><a href='/'>Mot de passe perdu ?</a></p>
+        {errors && values.user_mail === '' && values.user_password === '' ? <button onClick={handleChange} className='connexion-btn-inactif'> se connecter</button> : <button onClick={submit} className='connexion-btn-actif'>se connecter</button>}
+      </form>
+    </div>
   )
 }
 
