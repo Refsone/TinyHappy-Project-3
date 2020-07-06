1cd3a13c700f8bc6ce2b6a1f8f2f5c33e35edcf9
MANAUT <tristanmanaut@gmail.com>
2020-07-06 11:45:28 +0200

    change name label and input

--- src/components/onboarding/SignUp.js
+++ src/components/onboarding/SignUp.js
@@ -87,11 +87,11 @@ const SignUp = (props) => {
             <img src={visibleOne ? eyeOpen : eyeClosed} onClick={() => setVisibleOne(!visibleOne)} alt='' />
           </div>
           <input name='user_password' type={showTypeOne} id='user_password' value={user_password} onChange={handleChange} className='input-psw-default plholder bold-12px-grey' placeholder='**********' autoComplete='off' />
-          <label htmlFor='user_password' className='label-settings bold-12px-grey'>confirmer le mot de passe </label>
+          <label htmlFor='userConfirmPassword' className='label-settings bold-12px-grey'>confirmer le mot de passe </label>
           <div className='settings-container-eye'>
             <img src={visibleTwo ? eyeOpen : eyeClosed} onClick={() => setVisibleTwo(!visibleTwo)} alt='' />
           </div>
-          <input name='user_password' type={showTypeTwo} id='userConfirmPassword' value={userConfirmPassword} onChange={confirmPassword} className='input-psw-default plholder bold-12px-grey' placeholder='**********' autoComplete='off' />
+          <input name='userConfirmPassword' type={showTypeTwo} id='userConfirmPassword' value={userConfirmPassword} onChange={confirmPassword} className='input-psw-default plholder bold-12px-grey' placeholder='**********' autoComplete='off' />
           {successfull ? <ConfirmButton message='inscription reussie' confirm /> : btn}
           {error && <p className='signUp-error'>Une erreur s'est produite lors de l'inscription</p>}
           {redirect && <Redirect to='/onboarding/login' />}
