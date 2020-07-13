ce64b206b8715dd212664568451500fa11e95bd2
Jerôme Potié <49954831+jerkodeur@users.noreply.github.com>
2020-07-13 11:03:09 +0200

    Update AddShareMoment.js

--- src/components/share/AddShareMoment.js
+++ src/components/share/AddShareMoment.js
@@ -41,7 +41,7 @@ function AddShareMoment (props) {
     axios.get(`${backUrl}/users/${userId}/family`, {
       headers: { Authorization: `Bearer ${myToken}` }
     })
-      .then(res => setFamily(res.data) && console.log(res.data))
+      .then(res => setFamily(res.data))
     .catch(err => err)
   }
 
