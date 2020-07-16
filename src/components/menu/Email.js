3b37b978a4ad0310286df9dfa288cdd45b5da62b
Moni <monica_be@hotmail.fr>
2020-07-16 11:41:53 +0200

    adding regex

--- src/components/menu/Email.js
+++ src/components/menu/Email.js
@@ -1,30 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import useForm from './../onboarding/useForm'
-import validationLogIn from './../onboarding/validateLogin'
+import validationEmail from './validateEmail'
 
 import './../onboarding/Connexion.css'
 import './Password.css'
 import './Email.css'
 
+import { Redirect } from 'react-router-dom'
+
+const backUrl = process.env.REACT_APP_API_URL
+const myToken = (localStorage.getItem('x-access-token'))
+// const userId = localStorage.getItem('userId')
+
 const Email = () => {
-  const { handleChange, handleSubmit, values, errors } = useForm(submit, validationLogIn)
+  const { handleChange, handleSubmit, values, errors } = useForm(submit, validationEmail)
+
   function submit () {
-    console.log('sent succesfully')
+    axios.put(`${backUrl}/users/modify-email`, values, {
+      headers: { Authorization: `Bearer ${myToken}` }
+    })
+      .then(res => {
+        if (values.user_mail !== values.new_user_mail) {
+          console.log('Il y a une erreur dans votre email')
+        } else if (res.status === 400) {
+          console.log('votre email n\'existe pas')
+        }
+      })
   }
+
+/*   useEffect(() => {
+    if (redirect) {
+      setRedirect(true)
+    }
+  }, [redirect]) */
 
   return (
     <div className='settings-container-pwdmail'>
       <h1 className='settings-pwmail-title bold-16px-grey'>modification votre adresse email</h1>
       <form onSubmit={handleSubmit} className='general-form-connexion' noValidate>
         <label htmlFor='user_mail' name='user_mail' className='label-settings bold-12px-grey'>Nouvelle adresse mail</label>
-        <input name='user_mail' type='text' id='user_mail' value={values.email} onChange={handleChange} className={`${errors.user_mail ? 'input-email-error' : 'input-email plholder bold-12px-grey'}`} placeholder='**********' />
+
+        <input name='user_mail' type='text' id='user_mail' value={values.user_mail} onChange={handleChange} className={`${errors.user_mail ? 'input-pws-error' : 'input-psw-default plholder bold-12px-grey'}`} placeholder='**********' />
         {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
 
         <label htmlFor='user_mail' className='label-settings bold-12px-grey'>confirmation de la nouvelle adresse mail</label>
-        <input name='user_mail' type='text' id='user_mail' value={values.email} onChange={handleChange} className={`${errors.user_mail ? 'input-email-error' : 'input-email plholder bold-12px-grey'}`} placeholder='**********' />
-        {errors.user_mail && <p className='msg-error'>{errors.user_mail}</p>}
 
-        {errors && values.user_mail === '' ? <button type='submit' className='connexion-btn-inactif'>confirmer</button> : <button type='submit' className='connexion-btn-actif'>confirmer</button>}
+        <input name='new_user_mail' type='text' id='new_user_mail' value={values.new_user_mail} onChange={handleChange} className={`${errors.new_user_mail ? 'input-pws-error' : 'input-psw-default plholder bold-12px-grey'}`} placeholder='**********' />
+        {errors.new_user_mail && <p className='msg-error'>{errors.new_user_mail}</p>}
+
+        {errors ? <button type='submit' className='connexion-btn-inactif'>confirmer</button> : <button type='submit' className='connexion-btn-actif' onClick={(e) => submit(e)}>confirmer</button>}
+       {/*  {redirect && <Redirect to='/settings' />} */}
       </form>
     </div>
   )
