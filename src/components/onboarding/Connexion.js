8d4d8fe3c25285a50b52d67dcaa53b42ed1dd550
Auxence <auxence.blondel@gmail.com>
2020-07-24 09:48:46 +0200

    modify palette color to resolve bug on chrome and firefox

--- src/components/onboarding/Connexion.js
+++ src/components/onboarding/Connexion.js
@@ -50,6 +50,7 @@ const Connexion = (props) => {
         .then(data => {
           if (data) {
             localStorage.clear()
+            localStorage.removeItem('x-access-token')
             localStorage.setItem('x-access-token', data)
             localStorage.setItem('userId', jwt.decode(data).id)
             localStorage.setItem('userMail', jwt.decode(data).mail)
